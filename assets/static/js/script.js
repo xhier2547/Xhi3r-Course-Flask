1a1ff8e2399183205139bb68d54873d7ca09cc60
Xhi3r <aphilakjankaewdach@gmail.com>
2024-01-27 02:21:04 +0700

    add css and js for clients

--- assets/static/js/script.js
+++ assets/static/js/script.js
@@ -138,6 +138,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     /**
+     * Clients Slider
+     */
+    new Swiper('.clients-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 40
+            },
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 60
+            },
+            640: {
+                slidesPerView: 4,
+                spaceBetween: 80
+            },
+            992: {
+                slidesPerView: 6,
+                spaceBetween: 120
+            }
+        }
+    });
+
+
+
+    /**
      * Animation on scroll function and init
      */
     function aos_init() {
